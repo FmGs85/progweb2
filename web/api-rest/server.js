f3072d80da42618262631bad5a5abc31359c90c7
FmGs85 <fabiomelogs85@gmail.com>
2026-03-04 14:30:42 -0300

    aula 04 - adicionando controller, repository e routes

--- web/api-rest/server.js
+++ web/api-rest/server.js
@@ -1,8 +1,19 @@
-import app from "./src/app.js";
+import app from './src/index.js'
+// import conexao from './src/app/database/index.js'
 
-const port = 3000;
 
-//Escutando a porta 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+const port = 3000
+
+// //realizando a connection
+// conexao.connect((erro) => {
+//     if (erro) {
+//         console.log(`Falha ao conectar-se ao banco de dados: ${erro}`)
+//     } else {
+//         console.log("Conexão com Banco de Dados realizada com Sucesso")
+//         // Escutando a porta    
+        app.listen(port, () => {
+            console.log(`Servidor rodando http://localhost:${port}`)
+        })
+//     }
+// })
+
